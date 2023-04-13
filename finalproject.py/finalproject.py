b2ab5a491497b2fb6ebe0fd3d80b3bc7319351cc
Fatma Fadlelmola <fatma.fadlelmola@gmail.com>
2023-04-13 18:01:18 -0400

    added import statements

--- finalproject.py/finalproject.py
+++ finalproject.py/finalproject.py
@@ -1,11 +1,16 @@
+import re
+import argparse
+import random
+
 """A pizza simulation that outputs a summary of that days's inventory
 
 """
 class Pizza:
-    """Generates pizza for customer and removes topings from inventory 
+    """Generates pizza for customer and removes topings from inventory
+     
     
     """
-    pass
+    
 
 class Shop:
     """Generates class for each customer.
